423209b314614b751d6ab427bb467b9beb5ad714
Anton <anton2-2000@mail.ru>
2025-02-02 17:05:02 +0300

    dev adding tokenization

--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -5,14 +5,10 @@ import (
 	"math"
 )
 
-// Calculator интерфейс для всех типов калькуляторов
-type Calculator interface {
-	Compute(a, b float64, operator string) (float64, error)
-}
-
+// BasicCalculator - структура для выполнения операций
 type BasicCalculator struct{}
 
-// Compute выполняет операцию над числами в зависимости от оператора
+// Compute - метод для выполнения вычислений
 func (bc BasicCalculator) Compute(a, b float64, operator string) (float64, error) {
 	switch operator {
 	case "+":
@@ -27,13 +23,10 @@ func (bc BasicCalculator) Compute(a, b float64, operator string) (float64, error
 		}
 		return a / b, nil
 	case "%":
-		if b == 0 {
-			return 0, errors.New("division by zero")
-		}
 		return math.Mod(a, b), nil
 	case "^":
 		return math.Pow(a, b), nil
 	default:
-		return 0, errors.New("unknown operator")
+		return 0, errors.New("invalid operator")
 	}
 }
